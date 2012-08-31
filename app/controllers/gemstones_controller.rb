a503fe8adc975cfd47f957b4ce796372d81bd19e
Anders Lundström <anders@urkraft.se>
2012-08-31 03:23:42 +0200

    refactor gemstone-c

--- app/controllers/gemstones_controller.rb
+++ app/controllers/gemstones_controller.rb
@@ -7,47 +7,47 @@ load_and_authorize_resource
 #  end
 
   def harsten
-      @gemstone = Gemstone.find(params[:id])     
+      @gemstone = Gemstone.find params[:id]
   end
 
   def index
      @gemstones = Gemstone.search(params[:search]).includes(["links","chakras","energies","color"]).page(params[:page]).per(10)
-     #@gemstones = Gemstone.page params[:page]
+     # @gemstones = Gemstone.page params[:page]
      @antal = Gemstone.all.length
   end
-
-#  def show
-#    @gemstone = Gemstone.find(params[:id])
-#  end
 
   def new
     @gemstone = Gemstone.new
     3.times {@gemstone.properties.build}
   end
 
- # def edit
- #   @gemstone = Gemstone.find(params[:id])
- # end
-
   def create
-#     create!{gemstones_url}
+    # create!{gemstones_url}
     @gemstone = Gemstone.new(params[:gemstone])
     @gemstone.creator_id = current_user
 
-      if @gemstone.save
-        redirect_to(gemstones_url, :notice => 'Stenen skapades')
-      else
-        render :action => "new"
-      end
+    if @gemstone.save
+      redirect_to(gemstones_url, notice: 'Stenen skapades')
+    else
+      render action: 'new'
+    end
   end
 
   def update
      update!{gemstones_url}
   end
 
-#  def destroy
-#    @gemstone = Gemstone.find(params[:id])
-#    @gemstone.destroy
-#     destroy!{gemstones_url}
-#  end
+  # def show
+  #   @gemstone = Gemstone.find(params[:id])
+  # end
+
+  # def edit
+  #   @gemstone = Gemstone.find(params[:id])
+  # end
+
+  # def destroy
+  #   @gemstone = Gemstone.find(params[:id])
+  #   @gemstone.destroy
+  #   destroy!{gemstones_url}
+  # end
 end
